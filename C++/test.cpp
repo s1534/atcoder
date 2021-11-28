4a1e8d2c5495c4cfaee1674f776389bab320467f
mac_mishima <41721281+s1534@users.noreply.github.com>
2021-11-28 22:36:35 +0900

    テンプレート

---
+++ C++/test.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define ALL(x) (x).begin(), (x).end()
+using Graph = vector<vector<int> >;
+using P = pair<int, int>;
+
+int main()
+{
+}
